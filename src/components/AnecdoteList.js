f68fd1784431537648c0a5514b641280534e892b
Iikka <t8kuii00@students.oamk.fi>
2023-09-13 21:07:52 +0300

    Exercise 6.9

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -2,8 +2,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === '') {
+      return anecdotes
+    }
+    return anecdotes.filter(anec => anec.content.includes(filter))
+  })
 
   const onClickVote = (id) => {
     console.log('vote', id)
---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+export const setFilter = (filter) => {
+  return {
+    type: 'SET_FILTER',
+    payload: filter
+  }
+}
+
+const filterReducer = (state = '', action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'SET_FILTER': {
+      return action.payload
+    }
+    default:
+      return state
+  }
+}
+
+export default filterReducer
